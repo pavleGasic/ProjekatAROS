6a44340bf53287926b9dd746306f5a6532f876b2
PavleGasic <pavle.gasic7@gmail.com>
2022-02-28 23:41:37 +0100

    Dodate smoker funkcije

--- project.c
+++ project.c
@@ -32,6 +32,23 @@ bool smoker_tobacco_bool = false;
 bool smoker_match_bool = false;
 bool smoker_paper_bool = false;
 
+//potpisi funkcija
+void* agent(void* ptr);
+void* pusher_T(void* ptr);
+void* pusher_P(void* ptr);
+void* pusher_M(void* ptr);
+void* smoker_T(void* ptr);
+void* smoker_P(void* ptr);
+void* smoker_M(void* ptr);
+
+//main funkcija
+int main(int argc, char *argv[]){
+	
+	
+	return 0;
+}
+
+
 //funkcija agent
 void *agent(void* ptr){
 	while(1){
@@ -156,15 +173,88 @@ void* pusher_M(void* ptr){
 			printf("Pozovi u rad tobacco smoker-a.\n");
 			pthread_cond_signal(&smoker_thread_t);
 		}
+		
+		if(tobacco){
+			tobacco = false;
+			agent_bool = false;
+			smoker_paper_bool = true;
+			printf("Pozovi u rad paper smoker-a.\n");
+			pthread_cond_signal(&smoker_thread_p);
+			}
+		
 		pthread_mutex_unlock(&mutex1);
 		
 	}
 	return 0;
 }
 
-//main funkcija
-int main(int argc, char *argv[]){
+//smoker funkcije - pravljenje cigareta
+void* smoker_T(void* ptr){
 	
+	while(1){
+		pthread_mutex_lock(&mutex2);
+		
+		while(!smoker_tobacco_bool){
+				pthread_cond_wait(&smoker_thread_t, &mutex2);
+		}
+			
+		paper = false;
+		match = false;
+		smoker_tobacco_bool = false;
+		agent_bool = true;
+		
+		printf("Tobacco smoker pravi cigaretu...\n");
+		
+		pthread_mutex_unlock(&mutex2);
+		
+		printf("Tobacco smoker koristi cigaretu...\n");
+	}
 	
 	return 0;
+}
+
+void* smoker_M(void* ptr){
+	
+	while(1){
+		
+		pthread_mutex_lock(&mutex2);
+		
+		while(!smoker_match_bool){
+			pthread_cond_wait(&smoker_thread_m, &mutex2);
+		}
+		
+		tobacco = false;
+		paper = false;
+		smoker_match_bool = false;
+		agent_bool = true;
+		
+		printf("Match smoker pravi cigaretu...\n");
+		
+		pthread_mutex_unlock(&mutex2);
+		
+		printf("Match smoker koristi cigaretu...\n");
+		
+	}
+}
+
+void* smoker_P(void* ptr){
+	while(1){
+		
+		pthread_mutex_lock(&mutex2);
+		
+		while(!smoker_paper_bool){
+			pthread_cond_wait(&smoker_thread_p, &mutex2);
+		}
+		
+		tobacco = false;
+		match = false;
+		smoker_paper_bool = false;
+		agent_bool = true;
+		
+		printf("Paper smoker pravi cigaretu...\n");
+		
+		pthread_mutex_unlock(&mutex2);
+		
+		printf("Paper smoker koristi cigaretu...\n");
+	}
 }
